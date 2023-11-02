65479ee46426168fbdd53467a63bca9046a81642
FerryHuang <ferryhuang0814@163.com>
2023-11-02 21:30:09 +0800

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fast-reid",
+    version="0.1.0",
+    packages=find_packages(),
+    author="He, Lingxiao and Liao, Xingyu and Liu, Wu and Liu, Xinchen and Cheng, Peng and Mei, Tao",
+    license="Apache-2.0 license",
+    url="https://github.com/JDAI-CV/fast-reid",   # project home page, if any
+)
